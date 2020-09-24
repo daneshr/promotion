ae1e52cd876787d12dd7c9a6c457092e16619eb0
Danesh Razavi <daneshrazavi@gmail.com>
2020-09-24 22:26:04 +0200

    test cases for promotions were added

--- src/test/java/com/example/promotion/PromotionEngineTest.java
+++ src/test/java/com/example/promotion/PromotionEngineTest.java
@@ -12,33 +12,52 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.promotion.dto.Order;
 import com.example.promotion.service.impl.PromotionEngineImpl;
 
-
 @SpringBootTest
 public class PromotionEngineTest {
 
-    @Autowired
-    private PromotionEngineImpl promotionEngine;
+	@Autowired
+	private PromotionEngineImpl promotionEngine;
 
-    @Test
-    public void shouldThrowIllegalArgumentException_whenReceiveUnsupportedProduct() {
-    	Assertions.assertThrows(IllegalArgumentException.class, () -> {
-    	    promotionEngine.prepareOrder(Arrays.asList('Z'));
-    	  });
-    }
-    @Test
-    public void shouldThrowIllegalArgumentException_whenReceiveUnsupportedCart() {
-    	Assertions.assertThrows(IllegalArgumentException.class, () -> {
-    	    promotionEngine.prepareOrder(Arrays.asList('A','Z'));
-    	  });
-    }
-    @Test
-    public void shouldReturnProperOrder_whenReceiveScenario1() {
-    	Order order = promotionEngine.prepareOrder(Arrays.asList('A','B','C'));
-    	Assertions.assertTrue((new BigDecimal(100)).compareTo(order.getTotalPrice())==0);
-    	Assertions.assertIterableEquals(
-    			order.getItems().stream().map((item)->item.getDescription()).collect(Collectors.toList())
-    			, Arrays.asList("1 * A","1 * B","1 * C"));
-    }
+	@Test
+	public void shouldThrowIllegalArgumentException_whenReceiveUnsupportedProduct() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			promotionEngine.prepareOrder(Arrays.asList('Z'));
+		});
+	}
 
- 
+	@Test
+	public void shouldThrowIllegalArgumentException_whenReceiveUnsupportedCart() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			promotionEngine.prepareOrder(Arrays.asList('A', 'Z'));
+		});
+	}
+
+	@Test
+	public void shouldReturnProperOrder_whenReceiveScenario1() {
+		Order order = promotionEngine.prepareOrder(Arrays.asList('A', 'B', 'C'));
+		Assertions.assertTrue((new BigDecimal(100)).compareTo(order.getTotalPrice()) == 0);
+		Assertions.assertIterableEquals(
+				order.getItems().stream().map((item) -> item.getDescription()).collect(Collectors.toList()),
+				Arrays.asList("1 * A", "1 * B", "1 * C"));
+	}
+
+	@Test
+	public void shouldReturnProperOrder_whenReceiveScenario2() {
+		Order order = promotionEngine
+				.prepareOrder(Arrays.asList('A', 'B', 'A', 'B', 'A', 'B', 'A', 'B', 'A', 'B', 'C'));
+		Assertions.assertTrue((new BigDecimal(370)).compareTo(order.getTotalPrice()) == 0);
+		Assertions.assertIterableEquals(
+				order.getItems().stream().map((item) -> item.getDescription()).collect(Collectors.toList()),
+				Arrays.asList("3 of A\'s for 130", "2 of B\'s for 45", "2 of B\'s for 45", "2 * A", "1 * B", "1 * C"));
+	}
+
+	@Test
+	public void shouldReturnProperOrder_whenReceiveScenario3() {
+		Order order = promotionEngine.prepareOrder(Arrays.asList('B', 'A', 'B', 'B', 'A', 'B', 'A', 'B', 'C', 'D'));
+		Assertions.assertTrue((new BigDecimal(280)).compareTo(order.getTotalPrice()) == 0);
+		Assertions.assertIterableEquals(
+				order.getItems().stream().map((item) -> item.getDescription()).collect(Collectors.toList()),
+				Arrays.asList("3 of A\'s for 130", "2 of B\'s for 45", "2 of B\'s for 45", "1 * B", "C & D"));
+	}
+
 }
